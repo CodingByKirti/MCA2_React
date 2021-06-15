ce07c32197295149a7bf5d948fad1d498c61f42f
Kirti Sharma <kirtisharmaa.11@gmail.com>
2021-06-15 12:58:44 +0530

    commin150621

---
+++ router-ex/src/Main Pages/NavBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+export const NavBar = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/home/fridge">Fridge</NavLink>
+        </li>
+        <li>
+          <NavLink to="/home/ac">AC</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About Us</NavLink>
+        </li>
+        <li>
+          <NavLink exact to="/settings" activeStyle={{ color: "red" }}>
+            Setiings PAge
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
