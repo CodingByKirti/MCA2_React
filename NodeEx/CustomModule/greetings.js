12a53363bccdf7d592a2ba0d4a56d8766d87f6c3
CodingByKirti <75577040+CodingByKirti@users.noreply.github.com>
2021-07-22 15:30:51 +0530

    Add files via upload

---
+++ NodeEx/CustomModule/greetings.js
@@ -0,0 +1,14 @@
+/** */
+
+module.exports = {
+  sayHello: function (str) {
+    return "Hello " + str;
+  },
+  sayBye: function (str) {
+    return "Bye " + str;
+  },
+};
+
+module.exports.wishes = function (str) {
+  return "Best Wishes to " + str;
+};
